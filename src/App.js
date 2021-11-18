62ddf4da18c24b6f48f62423c2392186d0e31e6d
abrarfahim19 <fahimpatwari19@gmail.com>
2021-11-18 23:55:39 +0600

    This is first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{backgroundColor: 'steelblue'}}>
+      <h1> Let's go</h1>
+      <Here  name='Abrar'></Here>
+      <Here name= 'Fahim'></Here>
+      <Here name= 'Patwari'></Here>
+    </div>  
   );
 }
-
+function Here(props) {
+  const customStyle = {
+    color: 'white', border:'2px solid white', margin: '10px 20px', padding: '20px'
+  }
+  return (
+    <div style={customStyle}>
+      <h3 style={{color:'black'}}>Name: {props.name}</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, reiciendis?</p>
+    </div>
+    )
+}
 export default App;
