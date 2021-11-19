ffd994c8fcef9a4e76be741f530da11cea063d1a
abrarfahim19 <fahimpatwari19@gmail.com>
2021-11-19 22:53:37 +0600

    automating

--- src/App.js
+++ src/App.js
@@ -2,15 +2,56 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [{name:'Basic', price:'20$'},{name:'Standard', price:'40$'},{name:'Premium', price:'99$'}]
   return (
-    <div style={{backgroundColor: 'steelblue'}}>
-      <h1> Let's go</h1>
-      <Here  name='Abrar'></Here>
-      <Here name= 'Fahim'></Here>
-      <Here name= 'Patwari'></Here>
+    <div className="App">
+      <header className='App-header'>
+        <h1 style={{textAlign:'center'}}> Let's go</h1>
+
+        {/* first way to do this is by ... [ThreeDots]
+        second way to do this is by destructure
+        third way to do this is by array.map(product=>product.parameter)
+         */}
+ 
+        <h2>The list of products:</h2>
+         <ul>
+           {
+             products.map(product=> <li> {product.name +'And Price is '+ product.price} </li>)
+           }
+         </ul>
+        
+        {
+          products.map(product=> <Product product = {products} > </Product>)
+        }
+
+
+        <Here  name='Abrar'></Here>
+        <Here name= 'Fahim'></Here>
+        <Here name= 'Patwari'></Here>
+      </header>
     </div>  
   );
 }
+
+function Product(props){
+  const productStyle = {
+    textAlign:'center',
+    float: 'left',
+    border: '2px solid red',
+    backgroundColor: 'teal',
+    borderRadius: '5px',
+    height:'200px',
+    width:'200px',
+  }
+  return(
+    <div style={productStyle}>
+      <h3>{props.name}</h3>
+      <h2>{props.price}</h2>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+
 function Here(props) {
   const customStyle = {
     color: 'white', border:'2px solid white', margin: '10px 20px', padding: '20px'
@@ -22,4 +63,5 @@ function Here(props) {
     </div>
     )
 }
+
 export default App;
